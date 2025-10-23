7cab45e75a92de9364064d73b46bf83b921d38ae
TamHocDev <tamtrandinh3092004@gmail.com>
2025-10-23 11:23:56 +0700

    real-estate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       style={{ backgroundImage: "url('/header_img.png')" }}
       id="Header">
       <Navbar />
-      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+      <motion.div initial={{opacity: 0, y:100}} transition={{duration: 1.5}} whileInView={{opacity: 1, y: 0}} viewport={{once:true}} className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-x-3xl font-semibold pt-20">
           Explore homes that fill your dreams
         </h2>
@@ -20,7 +21,7 @@ const Header = () => {
             Contact Us
           </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { assets, projectsData } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Projects = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -26,7 +27,8 @@ const Projects = () => {
         setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1);
     }
   return (
-    <div
+    <motion.div
+      initial={{opacity: 0, x:-200}} transition={{duration: 1}} whileInView={{opacity: 1, x: 0}} viewport={{once:true}}
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
       id="Projects">
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
@@ -74,7 +76,7 @@ const Projects = () => {
       </div>
 
 
-    </div>
+    </motion.div>
   );
 }
 
